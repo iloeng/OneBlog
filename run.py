e698df652df776c4fa4eac5a2150d0601e6aa393
Leon <Liangz.org@gmail.com>
2021-06-15 23:01:40 +0800

    feat: add template filter to remove html tags

--- run.py
+++ run.py
@@ -10,10 +10,19 @@
    @Change:   2020.07.31
 -------------------------------------------------------------------------------
 """
+import re
+
 from apps.app import create_app
 from config import Config
 
 app = create_app()
+
+
+@app.template_filter('summary')
+def summary(data):
+    pattern = r'</?[?a-zA-Z]+[^><]*>|&[a-zA-Z]{1,10};'
+    return re.sub(pattern, ' ', data).strip()
+
 
 if __name__ == "__main__":
     app.run(debug=Config.DEBUG)
